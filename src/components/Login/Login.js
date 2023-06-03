07d4c831d3f7492f2583c295c5cdf5904faab364
ahbab1995 <islamahbab1996@yahoo.com>
2023-06-03 21:25:36 +0600

    login page onsubmit fun new code added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 export const Login = () => {
-  return (
-    <div className='container w-50 '>
-      <h2>Login</h2>
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-     
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+  const handleLogin = e => {
+    e.preventDefault()
+    const password = e.target.password.value;
+    const email = e.target.email.value;
+    console.log(password,email)
+  }
+  return (
+    <div className="container  ">
+      <h2>Login</h2>
+      <div className="w-25 mx-auto">
+        <Form onSubmit={handleLogin}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" name="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
-  )
-}
+  );
+};
